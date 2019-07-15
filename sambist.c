622fa46e1e3dbb8cd5def9b1590f3c42146c6141
zwode <49679191+zwode@users.noreply.github.com>
2019-07-15 22:18:36 +0300

    sad
    sad boys

--- sambist.c
+++ sambist.c
@@ -1,42 +1,5 @@
+
 #include "filit.h"
-
-
-// int     writing_map(char **map, int lim, char **fig, int bukva, int j) //i kol figur, j sdvig po ebalu
-// {
-//         int m_i;
-//         int m_j;
-//         int f_i;
-//         int f_j;
-// 		int check;
-
-//         m_i = 0;
-//         m_j = 0;
-//         f_i = 0;
-//         f_j = 0;
-//         if (m_i + bukva < lim)
-//         {
-//             if (m_j + j < lim && fig[f_i][f_j])
-//             {
-// 				check = write_fig(map, fig, f_i, lim, j);
-// 				print_map(map, lim);
-// 				bukva++;
-//                 if (check == 1)
-// 				{
-// 					f_i++;
-// 					writing_map(map, lim, fig, bukva, 0);
-// 				}
-// 				else if (check == -1)
-// 				{
-// 					bukva--;
-// 					del_last(map, lim, bukva);//i nomer bykvi
-// 					print_map(map, lim);
-// 					writing_map(map, lim, fig, bukva, ++j);
-// 				}
-//             }
-//         }
-// 		print_map(map, lim);
-// 		return (0);
-// }
 
 void	print_map(char **a, int r)
 {
@@ -47,7 +10,7 @@ void	print_map(char **a, int r)
 	{
 		if (i == 0)
 			write(1, "\n", 1);
-		write(1, a[i], 10);
+		write(1, a[i], r);
 		write(1, "\n", 1);
 		i++;
 	}
@@ -84,143 +47,193 @@ void    del_all(char **a, int r)
     while(i < r)
     {
         while(j < r)
-            a[i][j++] = '.';
+        {
+            a[i][j] = '.';
+            j++;
+        }
         i++;
         j = 0;
     }
-	print_map(a, r);
 }
 
 int		param(char **figure, int f_i)
 {
 		int counter;
-		char symbol;
+		int	i;
 
-		symbol = 'A' + f_i;
+		i = 0;
 		counter = 0;
-		while (figure[f_i][counter] != '\n')
-			counter++;
+		while (figure[f_i][i] != '\n')
+		{
+			//if (figure[f_i][i] != '.')
+				counter++;
+			i++;
+		}
 		return (counter);
 }
+
+/*
+int		write_fig(char **map, char **figure, int f_i, int lim, int sdvig)
+{
+		int j;
+		int f;
+		int	sdvig;
+		int len_cock;
+		char *ocko_loop1;
+
+		len_cock = ft_strlen(figure[f_i]);
+		sdvig = sdvig;
+		j = 0;
+		f = 0;
+		while (len_cock >= f && sdvig < lim  && (j < lim || figure[f_i][f] == '\n'))
+		{
+			if (figure[f_i][f] == '.'  && map[sdvig][j] != '.')
+			{
+				j++;
+				f++;
+			}
+   			//while (map[sdvig][j] != '.' && j < lim)
+			//	j++;
+			if (figure[f_i][f] == '\n')
+			{
+				f++; 
+				// if (figure[f_i][f] == '.')
+				// 	f++;
+				sdvig++;
+				j = j - param(figure, f_i);
+			}
+			if (figure[f_i][f] != '\0' && map[sdvig][j] == '.' && j < lim && (figure[f_i][f + 1] == '\n'	|| map[sdvig][j + 1] == '.'))// j + 1 hz, autism
+			{
+				map[sdvig][j] = figure[f_i][f];
+				f++;
+			}
+			if (j == lim && figure[f_i][f] != '\n')
+			{
+                f = 0;
+				j = -1;
+				sdvig++;
+			}
+			ocko_loop1 = map[sdvig];
+			j++;
+		}
+		if (f == len_cock)
+			return (1);
+		if (sdvig > lim && f < len_cock)
+			return (0);
+		if (j <= lim && figure[f_i][f] != '\n')
+			return (-1);
+	    return (-2);
+}
+*/
+
+
 
 int		write_fig(char **map, char **figure, int f_i, int lim, int sdvig)
 {
 		int j;
 		int f;
-		int	m_i;
 		int len_cock;
-		char *temp_look_1;
+		char *ocko_loop1;
 
 		len_cock = ft_strlen(figure[f_i]);
-		m_i = 0;
+		sdvig;
 		j = 0;
 		f = 0;
-		while (len_cock >= f && m_i < /*10*/lim  && (j < lim || figure[f_i][f] == '\n'))
-		{
-			if (figure[f_i][f] == '.'  && map[m_i + sdvig][j] != '.')
+		if ((sdvig = vert()) && (j = gor()))
+			while (j < lim)
 			{
-				j++;
-				f++;
+				while (figure[f_i][j] != '\0' && figure[f_i][j] != '\n' && figure[f_i][j])
+				{
+					map[sdvig][j] = figure[f_i][f];
+					j++;
+					f++;
+				}
 			}
-   			while (map[m_i + sdvig][j] != '.' && j < lim)
-				j++;
-			if (figure[f_i][f] == '\n')
-			{
-				f++;
-				m_i++;
-				j = j - param(figure, f_i);
-			}
-			if (figure[f_i][f] != '\0' && map[m_i + sdvig][j] == '.' && j < lim)
-			{
-				map[m_i + sdvig][j] = figure[f_i][f];
-				temp_look_1 = map[m_i + sdvig];
-				f++;
-			}
-			if (j == lim && figure[f_i][f] != '\n')
-			{
-				j = 0;
-				m_i++;
-			}
-			j++;
-		}
-		if (f == len_cock)//
+		if (f == len_cock)
 			return (1);
-		if (m_i == lim && j == lim && f < len_cock)
+		if (sdvig > lim && f < len_cock)
 			return (0);
-		if (j <= lim && figure[f_i][f] != '\n')//
-			return (-1);//
-	
-	return (-2);	
+		if (j <= lim && figure[f_i][f] != '\n')
+			return (-1);
+	    return (-2);
 }
 
 int		writing_map(char **map, int lim, char **fig, int i, int sdvig)//i eto nomer bukvy; int sdvig = po mape y po ebalu
 {
-	int 	m_i;
-	int 	m_j;
-	int 	f_i;
-	int 	f_j;
 	int 	check;
 
-	m_i = 0;
-    m_j = 0;
-    f_i = 0;
-    f_j = 0;
-	if (m_i + i < lim)
-	{
-		check = write_fig(map, fig, i, lim, sdvig);
-		print_map(map, lim);
-		i++;
-		if (!fig[i])
-			return (1);
-		if (check == 1)
-			writing_map(map, lim, fig, i, 0);
-		else if(check == -1)
-		{
-			i--;
-			del_last(map, lim, i);
-			print_map(map, lim);
-			writing_map(map, lim, fig, i, ++sdvig);
-		}
-		else if (check == 0)
-		{
-			del_all(map, lim);
-			print_map(map, lim);
-			writing_map(map, ++lim, fig, 0, sdvig);
-		}
-	}
+	check = write_fig(map, fig, i, lim, sdvig);
 	print_map(map, lim);
+	i++;
+	if (!fig[i] && check == 1)
+    {
+	    print_map(map, lim);
+		return (1);
+    }
+	if (check == 1)
+		writing_map(map, lim, fig, i, 0);
+	else if(check == -1)
+	{
+		i--;
+		del_last(map, lim, i);
+		writing_map(map, lim, fig, i, ++sdvig);
+	}
+	else if (check == 0)
+	{
+        i = 0;
+		del_all(map, lim + 1);
+		writing_map(map, ++lim, fig, i, 0);
+	}
 	return (1);
 }
 
 int		main()
 {
-	char	**a;
-	char	**f;
-	int		i;
-	int		j;
-	int		r;
+    char	        **a;
+    char	        **f;
+    int		        i;
+    int		        j;
+	static int		r;
 
-	a = malloc(sizeof(*a)*10);
+	a = malloc(sizeof(*a)*12);
 	i = 0;
-	while (i < 10)
-		a[i++] = ft_strnew(10);
+	while (i < 12)
+		a[i++] = ft_strnew(12);
 	i = 0;
-	while (i < 10)
+	while (i < 12)
 	{
 		j = 0;
-		while (j < 10)
+		while (j < 12)
 			a[i][j++] = '.';
 		i++;
 	}
 	f = malloc(sizeof(*a)*2);
-	f[0] = "AA\nA.\nA.\n";
-	f[1] = "BBBB\n";
-	f[2] = "CC\nCC\n";
-	//f[3] = "DD\nDD\n";
-	//f[4] = "I\nI\nI\nI\n";
+	//f[22] = "WW\nWW\n";
+    f[1] = ".B\n.B\nBB\n";
+	f[0] = "A.\nA.\nAA\n";
+	f[2] = "C.\nC.\nCC\n";
+    f[3] = "DD\nDD\n";
+    f[4] = "EE\nEE\n";
+    f[5] = "FF\nFF\n";
+    f[6] = "GG\nGG\n";
+    f[7] = "HH\nHH\n";
+    f[8] = "II\nII\n";
+    f[9] = "JJ.\n.JJ\n";
+    // f[10] = "KK\nKK\n";
+    // f[11] = "LL\nLL\n";
+	// f[12] = "MM\nMM\n";
+	// f[13] = "NN\nNN\n";
+    // f[14] = "OO\nOO\n";
+    // f[15] = "PP\nPP\n";
+    // f[16] = "QQ\nQQ\n";
+    // f[17] = "RR\nRR\n";
+    // f[18] = "SS\nSS\n";
+    // f[19] = "TT\nTT\n";
+    // f[20] = "UU\nUU\n";
+    // f[21] = "VV\nVV\n";
+
 	r = 3;
 	i = 0;
-	print_map(a, r);
 	writing_map(a, r, f, 0, 0);
 	return (4);
 }
